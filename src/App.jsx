effb7ac3fd56e313c422dfe8feb1432d25863a68
chaitanya-cloud08 <chaitanyabehlforwork@gmail.com>
2025-07-07 21:18:29 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TILpoc from './components/TILpoc';
-import PocViewer from './components/POCViewer';
+import PocViewer from './components/PocViewer';
 function App() {
   return (
     <BrowserRouter>
